5cf806c44032d31b13211506cef146505519e7a2
Eduardo <epl.eng18@uea.edu.br>
2024-06-15 17:35:30 -0400

    Develop 'Register User - Part 1' Screen

--- frontend/src/app/primeng/primeng.module.ts
+++ frontend/src/app/primeng/primeng.module.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core';
 import { MenubarModule } from 'primeng/menubar'
 import { ButtonModule } from 'primeng/button'
 import { InputTextModule } from 'primeng/inputtext'
+import { InputMaskModule } from 'primeng/inputmask';
 import { CarouselModule } from 'primeng/carousel'
+import { CalendarModule } from 'primeng/calendar';
 
 
 @NgModule({
@@ -12,13 +14,17 @@ import { CarouselModule } from 'primeng/carousel'
     MenubarModule,
     ButtonModule,
     InputTextModule,
-    CarouselModule
+    InputMaskModule,
+    CarouselModule,
+    CalendarModule
   ],
   exports: [
     MenubarModule,
     ButtonModule,
     InputTextModule,
-    CarouselModule
+    InputMaskModule,
+    CarouselModule,
+    CalendarModule
   ]
 })
 export class PrimengModule { }
